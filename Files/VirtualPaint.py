e216567bacb4f1c3c1fdce9db1768b64a3fbba05
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-08-11 23:58:44 +0530

    Files

--- Files/VirtualPaint.py
+++ Files/VirtualPaint.py
@@ -20,3 +20,9 @@ header = overlayList[0]
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
+
+#Main Loop
+while True:
+    success, img = cap.read() # Read frame from webcam
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
